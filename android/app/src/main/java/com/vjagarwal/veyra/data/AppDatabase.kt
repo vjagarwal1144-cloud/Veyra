9209031709cfc53e9263bc80a4881422108d9d7a
vjagarwal1144-cloud <vjagarwal1144@gmail.com>
2026-09-17 15:40:53 +0530

    Bump local database schema version

--- android/app/src/main/java/com/vjagarwal/veyra/data/AppDatabase.kt
+++ android/app/src/main/java/com/vjagarwal/veyra/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.vjagarwal.veyra.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [JourneyEntity::class], version = 1, exportSchema = false)
+@Database(entities = [JourneyEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun journeyDao(): JourneyDao
 }
