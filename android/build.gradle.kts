9a1f308590451c69235a630bdcfa2b7d352eb732
vjagarwal1144-cloud <vjagarwal1144@gmail.com>
2026-09-17 16:48:30 +0530

    Add Firebase Crashlytics Gradle plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.firebase.crashlytics") version "3.0.7" apply false
 }
