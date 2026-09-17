5f739607917fa5f0b8b85245b7e2a7bf256db1d6
vjagarwal1144-cloud <vjagarwal1144@gmail.com>
2026-09-17 15:46:59 +0530

    Add optional Firebase Google services plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
