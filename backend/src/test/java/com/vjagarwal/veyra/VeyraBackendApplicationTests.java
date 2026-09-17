88aca5892583cc453ec643e62202527741801013
vjagarwal1144-cloud <vjagarwal1144@gmail.com>
2026-09-17 15:49:36 +0530

    Add backend application context test

---
+++ backend/src/test/java/com/vjagarwal/veyra/VeyraBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vjagarwal.veyra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeyraBackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
